db3a2f9d8a869744b556d0eb4d163903b74f350c
Seung jun <poodle2405@gmail.com>
2021-03-21 23:06:05 +0900

    UPDATE dto

--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,1 +1,11 @@
-export class UpdateMovieDto {}
+import { IsNumber, IsString } from 'class-validator';
+
+export class UpdateMovieDto {
+  @IsString()
+  readonly title?: string;
+  @IsNumber()
+  readonly year?: number;
+
+  @IsString({ each: true })
+  readonly genres?: string[];
+}
